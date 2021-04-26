d901c7adacfcfdaf4621fe53d38aa3db9ff57b83
Ege <79145337+EgedotErcan@users.noreply.github.com>
2021-04-26 18:29:12 +0300

    Added physics and first line of code
    Unit 1 - Player Control Lesson 1.2.1- Lesson 1.2.9

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * 10);
     }
 }
