eabe0116544b34c5c7db0b7c3be8e6a4101ac87f
Ege <79145337+EgedotErcan@users.noreply.github.com>
2021-04-27 23:15:56 +0300

    Unit 1 Player Control first project CAR was finished
    Unit 1 - Player Control Lesson 1.4.1- Lesson 1.4.7

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20f;
-    // Start is called before the first frame update
+    private float speed = 14f;
+    private float turnSpeed = 45f;
+    private float horizontalInput;
+    private float forwardInput;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput  = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
     }
 }
