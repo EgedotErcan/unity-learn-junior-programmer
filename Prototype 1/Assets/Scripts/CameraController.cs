3c1db535a4dde788ae75e72818e66dc4be1b97e3
Ege <79145337+EgedotErcan@users.noreply.github.com>
2021-04-27 18:46:12 +0300

    Adjusted camera position
    Unit 1 - Player Control Lesson 1.3.1- Lesson 1.3.7

--- Prototype 1/Assets/Scripts/CameraController.cs
+++ Prototype 1/Assets/Scripts/CameraController.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    public float speed = 20f;
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5f, -7f);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position = player.transform.position + offset;
     }
 }
